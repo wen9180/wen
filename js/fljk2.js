5fa4fee6dee9f2bcc5e2efe389a50cdeb3b189dc
wen9180 <165254572+wen9180@users.noreply.github.com>
2024-05-24 10:38:20 +0800

    y

---
+++ js/fljk2.js
@@ -0,0 +1,13 @@
+[
+{"name": "最新合集",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/zxhj.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "古装三级",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/gzsj.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "麻豆资源",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/mdzy.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "玩偶姐姐",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/wojj.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "个人合集",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/grhj.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "暗里着迷",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/alzm.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "彩虹頻道",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/chpd.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "星星视界",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/xxsj.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+
+ {"name": "日韩资源",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/rjzy.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"},
+ {"name": "欧美资源",     "url": "https://bitbucket.org/qianruanhui/qrh/raw/a16431bc566c42c551d3274c89be4351ed91e319/omzy.txt&&&https://bkimg.cdn.bcebos.com/pic/9f510fb30f2442a79ec35f0adb43ad4bd013024b"}
+]
